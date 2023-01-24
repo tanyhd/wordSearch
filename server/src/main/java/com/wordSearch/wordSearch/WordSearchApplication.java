140790cabfa9308aaff117f90b1aeae4bb8a401f
Darren <darren243@hotmail.com>
2023-01-24 15:04:24 +0800

    Initial commit

---
+++ server/src/main/java/com/wordSearch/wordSearch/WordSearchApplication.java
@@ -0,0 +1,13 @@
+package com.wordSearch.wordSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordSearchApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/wordSearch/wordSearch/WordSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordSearch.wordSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
